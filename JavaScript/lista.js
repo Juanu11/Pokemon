dbdbc166608269b8896bd25c24c5c9e7d70ba88d
Juanu11 <j.urriagocalderon@gmail.com>
2025-04-03 22:02:57 +0000

    primer version

---
+++ JavaScript/lista.js
@@ -0,0 +1,17 @@
+function mostrarLista(pokemones){
+    const app = document.getElementById("app");
+    app.innerHTML = "Lista";
+    console.log(pokemones);
+    let Pokes = "";
+    for (let i = 0; i < pokemones.length; i++) {
+        let id = pokemones[i].url.split("/")[6];
+        Pokes += `
+        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${pokemones[i].name}')">
+            <p>#${id}</p>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
+            <p>${pokemones[i].name}</p>
+        </div>
+        `;
+    }
+    app.innerHTML = Pokes;
+}
